7a3e605696eac6191e24d55393cff206ebc16dd5
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-01-24 10:14:15 -0600

    changin implementation to show messages field or success in the payment

--- conekta_card/process.php
+++ conekta_card/process.php
@@ -1,11 +1,21 @@
 <?php
-
-$errors         = array();  	// array to hold validation errors
-$data 			= array(); 		// array to pass back data
-
-// validate the variables ======================================================
-	// if any of these variables don't exist, add an error to our $errors array
-
+if($_POST)
+{
+	
+	
+	//Sanitize input data using PHP filter_var().
+	$user_name		= filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+	$user_email		= filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+	$phone  	        = filter_var($_POST["phone"], FILTER_SANITIZE_NUMBER_INT);
+	$street	                = filter_var($_POST["street"], FILTER_SANITIZE_STRING);
+	$ext_number		= filter_var($_POST["ext_number"], FILTER_SANITIZE_STRING);
+	$int_number		= filter_var($_POST["int_number"], FILTER_SANITIZE_STRING);
+        $colony 		= filter_var($_POST["colony"], FILTER_SANITIZE_STRING);
+        $city		        = filter_var($_POST["city"], FILTER_SANITIZE_STRING);
+        $state		        = filter_var($_POST["state"], FILTER_SANITIZE_STRING);
+        $postal_code		= filter_var($_POST["postal_code"], FILTER_SANITIZE_STRING);
+        
+	
 	if (empty($_POST['name']))
 		$errors['name'] = 'El Nombre es Necesario.';
         
@@ -35,25 +45,17 @@ $data 			= array(); 		// array to pass back data
 
         if (empty($_POST['postal_code']))
 		$errors['postal_code'] = 'Ingresa el número de código postal';
-
-// return a response ===========================================================
-
-	// if there are any errors in our errors array, return a success boolean of false
-	if ( ! empty($errors)) {
-
-		// imprimimos los mensajes de error
-		$data['success'] = false;
-		$data['errors']  = $errors;
-                $data['message'] = 'error!';
-	} else {
-
+	
+	
+	if(!$send_mail)
+	{       
+                $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$user_name .'Feliciades tu pago ha sido aprobado,recibirás un correo con más detalles'));
+		die($output);
 		
-		// mostramos el mensaje de exito
-		$data['success'] = true;
-		$data['message'] = 'Success!';
-             
-                
+	}else{
+		
+		$output = json_encode(array('type'=>'error', 'text' => 'Lo sentimos tu pago no pudo ser procesado.'));
+		die($output);
 	}
-
-	// return all our data to an AJAX call
-	echo json_encode($data);
+}
+?>
--- js/pago.js
+++ js/pago.js
@@ -1,79 +1,57 @@
-
 $(document).ready(function() {
-
-	// process the form
-	$('form').submit(function(event) {
-
-		$('.form-group').removeClass('has-error'); // remove the error class
-		$('.help-block').remove(); // remove the error text
-
-		
-		// get the form data and get this data using jQuery
-		var formData = {
-			'name' 				: $('input[name=name]').val(),
-                        'email' 			: $('input[name=email]').val(),
-                        'phone' 		        : $('input[name=phone]').val(),
-                        'street' 		        : $('input[name=street]').val(),
-                        'ext_number' 		        : $('input[name=ext_number]').val(),
-                        'int_number' 		        : $('input[name=int_number]').val(),
-                        'colony' 		        : $('input[name=colony]').val(),
-                        'city' 				: $('input[name=city]').val(),
-                        'state' 			: $('input[name=state]').val(),
-			'postal_code' 	                : $('input[name=postal_code]').val(),
-                        'token' 	                : $('input[name=conektaTokenId]').val()
-		};
-
-		// procesamos el formulario
-		$.ajax({
-			type 		: 'POST',  // define the type of HTTP  to use (POST)
-			url 		: 'conekta_card/process.php', // the url where we want to POST
-			data 		: formData, // data object
-			dataType 	: 'json', // what type of data do we expect back from the server
-			encode 		: true
-		})
-			// using the done promise callback
-			.done(function(data) {
-
-				// log data to the console so we can see
-				console.log(data); 
-
-				// here we will handle errors and validation messages
-				if ( ! data.success) {
-					
-					// handle errors for name 
-					if (data.errors.name) {
-						$('#name-group').addClass('has-error'); // add the error class to show red input
-						$('#name-group').append('<div class="help-block">' + data.errors.name + '</div>'); // add the actual error message under our input
-					}
-
-					// handle errors for email 
-					if (data.errors.email) {
-						$('#email-group').addClass('has-error'); // add the error class to show red input
-						$('#email-group').append('<div class="help-block">' + data.errors.email + '</div>'); // add the actual error message under our input
-					}
-
-				
-
-				} else {
-
-					// ALL GOOD! just show the success message!
-					$('form').append('<div class="alert alert-success">' + data.message + '</div>');
-
-					
-
+    $("#submit_btn").click(function() { 
+       
+	    var proceed = true;
+       
+		$("#contact_form input[required=true], #contact_form textarea[required=true]").each(function(){
+			$(this).css('border-color',''); 
+			if(!$.trim($(this).val())){
+				$(this).css('border-color','red'); 
+				proceed = false; 
+			}
+			
+			var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
+			if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
+				$(this).css('border-color','red');  
+				proceed = false; 				
+			}	
+		});
+       
+        if(proceed) 
+        {
+			
+            post_data = {
+		        'name'                  : $('input[name=name]').val(),
+                        'email'                 : $('input[name=email]').val(),
+                        'phone'                 : $('input[name=phone]').val(),
+                        'street'                : $('input[name=street]').val(),
+                        'ext_number'            : $('input[name=ext_number]').val(),
+                        'int_number'            : $('input[name=int_number]').val(),
+                        'colony'                : $('input[name=colony]').val(),
+                        'city'                  : $('input[name=city]').val(),
+                        'state'                 : $('input[name=state]').val(),
+                        'postal_code'           : $('input[name=postal_code]').val(),
+                        'token'                 : $('input[name=conektaTokenId]').val()
+			};
+            
+            //Ajax post data to server
+            $.post('contact_me.php', post_data, function(response){  
+				if(response.type == 'error'){ //load json data from server and output message     
+					output = '<div class="error">'+response.text+'</div>';
+				}else{
+				    output = '<div class="success">'+response.text+'</div>';
+					//reset values in all input fields
+					$("#contact_form  input[required=true], #contact_form textarea[required=true]").val(''); 
+					$("#contact_form #contact_body").slideUp(); //hide form after success
 				}
-			})
-
-			// using the fail promise callback
-			.fail(function(data) {
-
-				// show any errors
-				// best to remove for production
-				console.log(data);
-			});
-
-		// stop the form from submitting the normal way and refreshing the page
-		event.preventDefault();
-	});
-
+				$("#contact_form #contact_results").hide().html(output).slideDown();
+            }, 'json');
+        }
+    });
+    
+    //reset previously set border colors and hide all message on .keyup()
+    $("#contact_form  input[required=true], #contact_form textarea[required=true]").keyup(function() { 
+        $(this).css('border-color',''); 
+        $("#result").slideUp();
+    });
 });
